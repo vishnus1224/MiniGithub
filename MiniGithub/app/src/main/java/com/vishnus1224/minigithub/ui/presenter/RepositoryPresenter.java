29dce842667e10cf57f92139b3af512e59f70a6a
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-06 21:22:10 +0530

    interface to provide callback when fetching repositories is done

--- MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/presenter/RepositoryPresenter.java
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/presenter/RepositoryPresenter.java
@@ -1,14 +1,21 @@
 package com.vishnus1224.minigithub.ui.presenter;
 
+import com.vishnus1224.minigithub.model.Repository;
+import com.vishnus1224.minigithub.ui.view.BaseView;
+
+import java.util.List;
+
 /**
  * Handles operations related to repositories.
  * Created by Vishnu on 2/6/2016.
  */
 public class RepositoryPresenter implements Presenter {
 
-    @Override
-    public void init() {
+    private BaseView view;
 
+    @Override
+    public void init(BaseView view) {
+        this.view = view;
     }
 
     @Override
@@ -23,6 +30,23 @@ public class RepositoryPresenter implements Presenter {
 
     @Override
     public void destroy() {
+
+    }
+
+    /**
+     * Fetch all repositories that match the given name.
+     * @param repositoryName The name of the repository to search.
+     */
+    public void getRepositories(String repositoryName){
+
+
+    }
+
+    public interface RepositoryFetchListener {
+
+        void onSuccess(List<Repository> repositories);
+
+        void onFailure(String message);
 
     }
 }
