2f2cd73c926b6e0946e024ff01c55bf9f409ec19
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-06 15:21:23 +0530

    derived main activity from the base activity

--- MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/activity/MainActivity.java
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/activity/MainActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 
 import com.vishnus1224.minigithub.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 }
