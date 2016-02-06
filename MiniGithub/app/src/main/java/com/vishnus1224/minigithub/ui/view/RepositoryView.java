8c3b46e35dd36d3e12bf293aa9706c3c6b929dd0
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-06 21:21:13 +0530

    method to show the list of repositories to the user

--- MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/view/RepositoryView.java
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/view/RepositoryView.java
@@ -1,9 +1,15 @@
 package com.vishnus1224.minigithub.ui.view;
 
+import com.vishnus1224.minigithub.model.Repository;
+
+import java.util.List;
+
 /**
  * Created by Vishnu on 2/6/2016.
  */
 public interface RepositoryView extends BaseView {
 
+    //update the ui using the list of repositories provided.
+    void showRepositories(List<Repository> repositoryList);
 
 }
