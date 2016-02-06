918afc77042cdd8b9fe562ff1ba948aadf436742
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-06 20:35:58 +0530

    removed unused import

--- MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/fragment/UsersFragment.java
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/fragment/UsersFragment.java
@@ -2,7 +2,6 @@ package com.vishnus1224.minigithub.ui.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
