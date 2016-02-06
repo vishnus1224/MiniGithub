a6e2c92958ad255b54addd2161a3d6e2d0977376
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-06 15:08:34 +0530

    moved main activity to the ui package

--- MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/activity/MainActivity.java
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.vishnus1224.minigithub;
+package com.vishnus1224.minigithub.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.vishnus1224.minigithub.R;
 
 public class MainActivity extends AppCompatActivity {
 
