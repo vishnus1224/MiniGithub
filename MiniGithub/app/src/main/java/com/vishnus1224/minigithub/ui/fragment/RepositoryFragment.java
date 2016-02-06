8a65fe5d871d89a572f03241a3224ec1f0750018
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-06 21:01:43 +0530

    initialized the presenter and called its lifecycle methods

--- MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/fragment/RepositoryFragment.java
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/fragment/RepositoryFragment.java
@@ -7,11 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.vishnus1224.minigithub.R;
+import com.vishnus1224.minigithub.ui.presenter.Presenter;
+import com.vishnus1224.minigithub.ui.presenter.RepositoryPresenter;
+import com.vishnus1224.minigithub.ui.view.RepositoryView;
 
 /**
  * Created by Vishnu on 2/6/2016.
  */
-public class RepositoryFragment extends BaseFragment {
+public class RepositoryFragment extends BaseFragment implements RepositoryView {
+
+    private Presenter presenter;
 
     @Nullable
     @Override
@@ -23,7 +28,53 @@ public class RepositoryFragment extends BaseFragment {
     }
 
     @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        presenter = new RepositoryPresenter();
+
+        //initialize the presenter by passing it the view.
+        presenter.init(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        presenter.resume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        presenter.pause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        presenter.destroy();
+    }
+
+    @Override
     public void fetchData() {
+
+    }
+
+    @Override
+    public void showProgress() {
+
+    }
+
+    @Override
+    public void hideProgress() {
+
+    }
+
+    @Override
+    public void showError(String message) {
 
     }
 }
