6cfb71ed4895913de63e5625beb105ab0c75bd2f
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-06 20:52:54 +0530

    created the repository presenter

---
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/presenter/RepositoryPresenter.java
@@ -0,0 +1,28 @@
+package com.vishnus1224.minigithub.ui.presenter;
+
+/**
+ * Handles operations related to repositories.
+ * Created by Vishnu on 2/6/2016.
+ */
+public class RepositoryPresenter implements Presenter {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
