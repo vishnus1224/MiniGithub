ba1533470218e3648f78aa548cac63c37dd180ab
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-06 20:20:21 +0530

    added code to generate a list of fragments using the fragment generator

--- MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/activity/HomeActivity.java
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/activity/HomeActivity.java
@@ -2,11 +2,20 @@ package com.vishnus1224.minigithub.ui.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 
 import com.vishnus1224.minigithub.R;
 import com.vishnus1224.minigithub.config.TabConfig;
+import com.vishnus1224.minigithub.generator.FragmentGenerator;
 import com.vishnus1224.minigithub.ui.adapter.MainTabsPagerAdapter;
+import com.vishnus1224.minigithub.ui.fragment.CodeFragment;
+import com.vishnus1224.minigithub.ui.fragment.IssuesFragment;
+import com.vishnus1224.minigithub.ui.fragment.RepositoriesFragment;
+import com.vishnus1224.minigithub.ui.fragment.UsersFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends BaseActivity {
 
@@ -39,7 +48,7 @@ public class HomeActivity extends BaseActivity {
     private void setupViewPager() {
 
         //create the adapter for the view pager by getting values from the tab config.
-        mainTabsPagerAdapter = new MainTabsPagerAdapter(getSupportFragmentManager(), TabConfig.getPagerFragments(), TabConfig.tabNames, TabConfig.NUMBER_OF_TABS);
+        mainTabsPagerAdapter = new MainTabsPagerAdapter(getSupportFragmentManager(), getPagerFragments(), TabConfig.tabNames, TabConfig.NUMBER_OF_TABS);
 
         homeTabViewPager.setAdapter(mainTabsPagerAdapter);
     }
@@ -51,6 +60,27 @@ public class HomeActivity extends BaseActivity {
 
         //setup the tab using the contents of the view pager.
         homeTabLayout.setupWithViewPager(homeTabViewPager);
+
+    }
+
+    /**
+     * Get the fragments required for setting up the tabs using the fragment generator.
+     * @return List of fragments.
+     */
+    private List<Fragment> getPagerFragments(){
+
+        List<Fragment> fragments = new ArrayList<>();
+
+        fragments.add(FragmentGenerator.generateFragment(RepositoriesFragment.class));
+
+        fragments.add(FragmentGenerator.generateFragment(CodeFragment.class));
+
+        fragments.add(FragmentGenerator.generateFragment(IssuesFragment.class));
+
+        fragments.add(FragmentGenerator.generateFragment(UsersFragment.class));
+
+        return fragments;
+
 
     }
 }
