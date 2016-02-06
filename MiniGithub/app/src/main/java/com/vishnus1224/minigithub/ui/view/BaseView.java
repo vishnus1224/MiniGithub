1a5071bc423ea34e5df7495f954ba057ea890526
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-06 15:40:48 +0530

    base view abstraction for showing, hiding progress and for showing error message

---
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/view/BaseView.java
@@ -0,0 +1,13 @@
+package com.vishnus1224.minigithub.ui.view;
+
+/**
+ * Created by Vishnu on 2/6/2016.
+ */
+public interface BaseView {
+
+    void showProgress();
+
+    void hideProgress();
+
+    void showError(String message);
+}
