166643f9b5069587d79d8cba4f6670ec66afa651
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-07 13:02:14 +0530

    changed from http to https

--- MiniGithub/app/src/main/java/com/vishnus1224/minigithub/webservice/GithubService.java
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/webservice/GithubService.java
@@ -1,9 +1,21 @@
 package com.vishnus1224.minigithub.webservice;
 
+import com.vishnus1224.minigithub.model.Repository;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
 /**
  * Created by Vishnu on 2/6/2016.
  */
 public interface GithubService {
 
-    String BASE_URL = "http://api.github.com";
+    String BASE_URL = "https://api.github.com/";
+
+    @GET("search/repositories")
+    Call<List<Repository>> fetchRepositories(@Query("q") String repositoryName);
 }
