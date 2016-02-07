53fb71647730fc6f7a3bc8737f283908d169024f
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-07 13:21:20 +0530

    updated repo model

--- MiniGithub/app/src/main/java/com/vishnus1224/minigithub/model/Repository.java
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/model/Repository.java
@@ -1,10 +1,50 @@
 package com.vishnus1224.minigithub.model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Vishnu on 2/6/2016.
  */
 public class Repository {
 
     private int id;
+
+    @SerializedName("full_name")
     private String name;
+
+    private String description;
+
+    private String language;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
 }
