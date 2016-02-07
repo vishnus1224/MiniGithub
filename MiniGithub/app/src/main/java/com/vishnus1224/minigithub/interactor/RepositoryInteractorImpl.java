7f901c82ca2ea6bdd7a3aeac0154f2b098eaa3a8
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-07 13:03:24 +0530

    concrete implementation of repository interactor

--- MiniGithub/app/src/main/java/com/vishnus1224/minigithub/interactor/RepositoryInteractorImpl.java
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/interactor/RepositoryInteractorImpl.java
@@ -1,12 +1,54 @@
 package com.vishnus1224.minigithub.interactor;
 
+import com.vishnus1224.minigithub.manager.RepositoryManager;
+import com.vishnus1224.minigithub.model.Repository;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 /**
  * Created by Vishnu on 2/7/2016.
  */
 public class RepositoryInteractorImpl implements RepositoryInteractor {
 
+    private RepositoryInteractionListener repositoryInteractionListener;
+
     @Override
     public void fetchRepositories(String repositoryName, RepositoryInteractionListener repositoryInteractionListener) {
 
+        this.repositoryInteractionListener = repositoryInteractionListener;
+
+        //get the webservice and make the fetch repositories call
+        Call<List<Repository>> call = RepositoryManager.getWebService().fetchRepositories(repositoryName);
+
+        //fetch on a background thread and pass a callback.
+        call.enqueue(callback);
+
     }
+
+    private Callback<List<Repository>> callback = new Callback<List<Repository>>() {
+        @Override
+        public void onResponse(Call<List<Repository>> call, Response<List<Repository>> response) {
+
+            if(response.isSuccess()){
+
+                //call the success method on the listener.
+                repositoryInteractionListener.onSuccess(response.body());
+
+            }else{
+
+                repositoryInteractionListener.onFailure(response.message());
+            }
+
+        }
+
+        @Override
+        public void onFailure(Call<List<Repository>> call, Throwable t) {
+
+            repositoryInteractionListener.onFailure(t.getMessage());
+        }
+    };
 }
