ca3e9b12d118e6e8c55e56548519ddc539d1bac6
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-08 21:10:28 +0530

    added method to load more repositories

--- MiniGithub/app/src/main/java/com/vishnus1224/minigithub/interactor/RepositoryInteractor.java
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/interactor/RepositoryInteractor.java
@@ -9,7 +9,19 @@ import java.util.List;
  */
 public interface RepositoryInteractor {
 
+    /**
+     * Executed when user searches for a repository the 1st time.
+     * @param repositoryName The name of the repository.
+     * @param repositoryInteractionListener Callback.
+     */
     void fetchRepositories(String repositoryName, RepositoryInteractionListener repositoryInteractionListener);
+
+    /**
+     * Executed when user clicks the load more button.
+     * @param repositoryName The name of the repository.
+     * @param repositoryInteractionListener Callback.
+     */
+    void loadMoreRepositories(String repositoryName, RepositoryInteractionListener repositoryInteractionListener);
 
     interface RepositoryInteractionListener{
 
