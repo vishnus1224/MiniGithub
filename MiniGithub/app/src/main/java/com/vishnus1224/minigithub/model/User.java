61baf1fa84ca440b36285ff7955a4f54ba4ebce2
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-13 15:08:19 +0530

    user model class

---
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/model/User.java
@@ -0,0 +1,74 @@
+package com.vishnus1224.minigithub.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Vishnu on 2/13/2016.
+ */
+public class User implements Parcelable {
+
+    private int id;
+
+    @SerializedName("login")
+    private String username;
+
+    @SerializedName("avatar_url")
+    private String avatarURL;
+
+    protected User(Parcel in) {
+        id = in.readInt();
+        username = in.readString();
+        avatarURL = in.readString();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAvatarURL() {
+        return avatarURL;
+    }
+
+    public void setAvatarURL(String avatarURL) {
+        this.avatarURL = avatarURL;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeString(username);
+        parcel.writeString(avatarURL);
+    }
+}
