a61f5a53116d1499635e89d206147695b10f6b52
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-13 12:27:23 +0530

    new date formats added, method to format date added

--- MiniGithub/app/src/main/java/com/vishnus1224/minigithub/utility/Utils.java
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/utility/Utils.java
@@ -1,9 +1,24 @@
 package com.vishnus1224.minigithub.utility;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Vishnu on 2/13/2016.
  */
 public final class Utils {
+
+    public static final SimpleDateFormat githubDateFormat;
+
+    public static final SimpleDateFormat appDateFormat;
+
+    static {
+
+        githubDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+
+        appDateFormat = new SimpleDateFormat("MMMM, d, yyyy");
+
+    }
 
     private Utils(){
         
@@ -11,5 +26,21 @@ public final class Utils {
 
     //key for parceling repository between activities.
     public static final String KEY_REPOSITORY = "repository";
+
+    //Formats the input dateString in the format specified by the output date format.
+    public static String formatDate(String dateString, SimpleDateFormat inputDateFormat, SimpleDateFormat outputDateFormat){
+
+        try {
+
+            Date date = inputDateFormat.parse(dateString);
+
+            return outputDateFormat.format(date);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 
 }
