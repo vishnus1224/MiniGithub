127fb167c07a7ad5d379dfbae9d972fa82bea73f
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-13 10:58:18 +0530

    utility class

---
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/utility/Utils.java
@@ -0,0 +1,15 @@
+package com.vishnus1224.minigithub.utility;
+
+/**
+ * Created by Vishnu on 2/13/2016.
+ */
+public final class Utils {
+
+    private Utils(){
+        
+    }
+
+    //key for parceling repository between activities.
+    public static final String KEY_REPOSITORY = "repository";
+
+}
