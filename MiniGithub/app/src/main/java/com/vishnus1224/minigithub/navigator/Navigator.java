be36c0ed509b80334b1c1e537d360b1799f45da4
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-02-13 10:55:51 +0530

    added method to navigate to the specified activity

--- MiniGithub/app/src/main/java/com/vishnus1224/minigithub/navigator/Navigator.java
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/navigator/Navigator.java
@@ -1,5 +1,10 @@
 package com.vishnus1224.minigithub.navigator;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
+
+
 /**
  * Created by Vishnu on 2/6/2016.
  */
@@ -7,6 +12,12 @@ public enum  Navigator {
 
     INSTANCE;
 
+    public void navigateToActivity(Context context, Class activityClass, Parcelable parcel, String parcelKey){
 
+        Intent intent = new Intent(context, activityClass);
+        intent.putExtra(parcelKey, parcel);
+        context.startActivity(intent);
+
+    }
 
 }
