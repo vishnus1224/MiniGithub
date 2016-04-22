6fe3d790e1aacbc61020cca4c39102838686cbd1
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-22 21:10:31 +0530

    fragment component and its corresponding module for injecting dependencies required in a fragment

---
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/di/component/FragmentComponent.java
@@ -0,0 +1,17 @@
+package com.vishnus1224.minigithub.di.component;
+
+import com.vishnus1224.minigithub.di.module.FragmentModule;
+import com.vishnus1224.minigithub.di.scope.PerFragment;
+import com.vishnus1224.minigithub.ui.fragment.RepositoryFragment;
+
+import dagger.Component;
+
+/**
+ * Created by Vishnu on 4/20/2016.
+ */
+@PerFragment
+@Component(modules = FragmentModule.class)
+public interface FragmentComponent {
+
+    void inject(RepositoryFragment repositoryFragment);
+}
---
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/di/module/FragmentModule.java
@@ -0,0 +1,20 @@
+package com.vishnus1224.minigithub.di.module;
+
+import com.vishnus1224.minigithub.di.scope.PerFragment;
+import com.vishnus1224.minigithub.interactor.RepositoryInteractor;
+import com.vishnus1224.minigithub.interactor.RepositoryInteractorImpl;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by Vishnu on 4/20/2016.
+ */
+@Module
+public class FragmentModule {
+
+    @Provides @PerFragment
+    RepositoryInteractor repositoryInteractor(RepositoryInteractorImpl repositoryInteractorImpl){
+        return repositoryInteractorImpl;
+    }
+}
