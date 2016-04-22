24aa5d602169bccdaa4bb4e9003207479ae4674a
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-22 21:14:29 +0530

    created the application class and linked it through the manifest

---
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/MiniGithub.java
@@ -0,0 +1,14 @@
+package com.vishnus1224.minigithub;
+
+import android.app.Application;
+
+/**
+ * Created by Vishnu on 4/22/2016.
+ */
+public class MiniGithub extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
