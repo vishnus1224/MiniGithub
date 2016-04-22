7b7fb09a5eb59574738782ad8fd1d8e9cc65e443
Vishnu Shirodkar <vishnus1224@gmail.com>
2016-04-22 23:15:16 +0530

    method for accessing the application instance and its net component

--- MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/fragment/BaseFragment.java
+++ MiniGithub/app/src/main/java/com/vishnus1224/minigithub/ui/fragment/BaseFragment.java
@@ -7,6 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.vishnus1224.minigithub.MiniGithub;
+import com.vishnus1224.minigithub.di.component.NetComponent;
+
 /**
  * Created by Vishnu on 2/6/2016.
  */
@@ -18,5 +21,21 @@ public abstract class BaseFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    /**
+     * Get the net component from the application class.
+     * @return The net component initialized in application class.
+     */
+    public NetComponent getNetComponent(){
+        return getApplication().getNetComponent();
+    }
+
+    /**
+     * Get application instance.
+     * @return The application instance cast to MiniGithub.
+     */
+    public MiniGithub getApplication(){
+        return (MiniGithub) getActivity().getApplication();
     }
 }
